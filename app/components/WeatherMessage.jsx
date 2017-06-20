a7d28104728f898a1758e1178a51b932b51138f7
David <david@tiqtok.com>
2017-06-20 12:37:10 -0700

    Refactors remaining stateless components to use =>

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,12 +1,10 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-  render: function(){
-    var {location, temp} = this.props;
-    return (
-        <h3>The Weather is {temp} degs is {location}</h3>
-    )
-  }
-});
+var WeatherMessage = (props) => {
+  var {location, temp} = props;
+  return (
+      <h3>The Weather is {temp} degs is {location}</h3>
+  )
+}
 
 module.exports = WeatherMessage;
