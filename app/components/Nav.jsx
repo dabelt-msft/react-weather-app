d750a7ea0f34b1ee74eba842007a786fa99b2f5e
David <david@tiqtok.com>
2017-06-21 15:58:29 -0700

    Style nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,16 +1,43 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = (props) => {
+var Nav = React.createClass({
+  onSearch: function(e){
+    e.preventDefault();
+    alert('not yet wired')
+  },
+  render: function(){
     return (
-      <div>
-          <h2>Nav Component</h2>
-          {/*once used Link instead of indexLink but changed because nesting affected activeStyle*/}
-          <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-          <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-          <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li menu-text>Weather App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search Weather"/>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
       </div>
     )
-}
+  }
+});
 
 module.exports = Nav;
