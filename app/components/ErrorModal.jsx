844175228fc78d9fcd991ef9a70a0025703320e4
David <david@tiqtok.com>
2017-06-21 17:23:41 -0700

    Updates Error Modal

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,22 +1,33 @@
 var React = require('react');
 
 var ErrorModal = React.createClass({
-  componentDidMount: function(){
+  getDefaultProps: function () {
+    return {
+      title: 'Error'
+    };
+  },
+  propTypes: {
+      title: React.PropTypes.string,
+      message: React.PropTypes.string.isRequired
+  },
+  componentDidMount: function () {
     var modal = new Foundation.Reveal($('#error-modal'));
     modal.open();
   },
-  render: function(){
+  render: function () {
+    var {title, message} = this.props;
+
     return (
       <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-        <h4>Error:</h4>
-        <p>Error Message</p>
+        <h4>{title}</h4>
+        <p>{message}</p>
         <p>
           <button className="button hollow" data-close="">
-              Okay
+            Okay
           </button>
         </p>
       </div>
-    )
+    );
   }
 });
 
